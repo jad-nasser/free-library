940478eca43a43ff836c3cff9b3381813897db5d
jad-nasser <82750984+jad-nasser@users.noreply.github.com>
2022-07-05 11:45:19 +0200

    checkLogin.js completed

---
+++ client/free-library/src/checkLogin.js
@@ -0,0 +1,22 @@
+//importing modules
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+const navigate = useNavigate();
+
+//there are two pages types: "publisher" pages are pages only for publishers, and "default" pages are
+//pages that are for the normal users that are not publishers.
+//the aim of this function is to check if the user is in the correct page type and if not it navigates the
+//user to other page.
+const checkLogin = (pageType) => {
+  axios
+    .get(process.env.REACT_APP_BASE_URL + "/publishers/check-login")
+    //the user is signed in as a publisher
+    .then(() => {
+      if (pageType !== "publisher") navigate("/publisher/home");
+    })
+    //the user is not signed in as a publisher
+    .catch(() => {
+      if (pageType !== "default") navigate("/home");
+    });
+};
+export default checkLogin;
